a040a81811595a1f5010dac634a3d87826d50047
Taylan Pince <taylanpince@gmail.com>
2010-01-19 17:27:39 -0500

    Initial commit with a working wiki system

---
+++ build/wiki/settings.py
@@ -0,0 +1,59 @@
+import os
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Taylan Pince', 'taylanpince@gmail.com'),
+)
+
+MANAGERS = ADMINS
+
+TIME_ZONE = 'Canada/Eastern'
+
+USE_I18N = True
+
+SITE_ID = 1
+
+MEDIA_ROOT = os.path.join(os.path.realpath(os.path.dirname(__file__)), 'media/')
+MEDIA_URL = '/media/'
+
+SECRET_KEY = '_usq1e@4x-$ul^6s1*2l0(@daeesnfx27^0k%sd&lp2(olkf6g'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.media',
+)
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.gzip.GZipMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.doc.XViewMiddleware',
+)
+
+ROOT_URLCONF = 'wiki.urls'
+
+TEMPLATE_DIRS = (
+    os.path.join(os.path.realpath(os.path.dirname(__file__)), 'templates/'),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.contenttypes',
+    
+    'doc_wiki',
+)
+
+try:
+    from settings_local import *
+except:
+    pass
+
+ADMIN_MEDIA_PREFIX = MEDIA_URL + 'admin/'
